1ca55c4a48abf37bf4dcb3faf5ee6c10baae8d06
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-13 09:49:07 +0200

    Delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
     
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-      
-      </div>
-    
     </>
   )
 }
