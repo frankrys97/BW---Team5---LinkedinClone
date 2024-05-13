79ed14f2f42e20e332abc42d27de64ec5bdd6b86
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-13 10:30:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
+import { BrowserRouter, Routes } from 'react-router-dom'
+import './'
 function App() {
   return (
     <BrowserRouter>
